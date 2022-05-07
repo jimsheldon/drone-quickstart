887d0502c0038da41be2ddb0bc34ba7549170df3
L00170155 <40305076+brianmcnic@users.noreply.github.com>
2022-05-07 16:09:03 +0100

    First commit

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=
-export DRONE_GITHUB_CLIENT_SECRET=
-export DRONE_GITHUB_ADMIN=
-export DRONE_SERVER_HOST=
+export DRONE_GITHUB_CLIENT_ID=b79751d6eb16b50bc1ea
+export DRONE_GITHUB_CLIENT_SECRET=93b14cd52551fc1f7b77652fe11283bb38307158
+export DRONE_GITHUB_ADMIN=L00170155
+export DRONE_SERVER_HOST=0940-176-61-33-92.eu.ngrok.io
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
