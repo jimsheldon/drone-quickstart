f23264596673d6335533e024f6d3a39f6978990f
VladislavZelenin <Vladislav.Zelenin@livevision.com.ua>
2023-05-21 15:30:25 +0300

    test

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=
-export DRONE_GITHUB_CLIENT_SECRET=
-export DRONE_GITHUB_ADMIN=
-export DRONE_SERVER_HOST=
+export DRONE_GITHUB_CLIENT_ID=a2ec3a80a98661e4fac7
+export DRONE_GITHUB_CLIENT_SECRET=1fdb09293274b094ba17abfa4db752412c823d4b
+export DRONE_GITHUB_ADMIN=vladislav2402
+export DRONE_SERVER_HOST=c8d5-194-156-250-153.ngrok-free.app
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
